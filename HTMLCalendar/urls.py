8de057aa34933433306d92cbc3e11aff721d9afb
simjiwon <wldnjs0919@likelion.org>
2019-07-31 23:07:59 +0900

    firstcommittttt

---
+++ HTMLCalendar/urls.py
@@ -0,0 +1,25 @@
+"""HTMLCalendar URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+import events.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', events.views.calendar_view, name="calendar"),
+    path('event/new/', events.views.event, name="new"),
+    path('event/edit/<int:event_id>', events.views.event, name="edit"),
+]
