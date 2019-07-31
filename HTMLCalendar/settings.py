66d16250ce97b423d052285ef5fcc26125bf2e7d
simjiwon <wldnjs0919@likelion.org>
2019-07-31 23:18:09 +0900

    second

--- HTMLCalendar/settings.py
+++ HTMLCalendar/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r*&5y9l$am*z5%nq&iiqyyomanet7m!8j_v4_l%qj^uaahj9ch'
+# SECRET_KEY = 'r*&5y9l$am*z5%nq&iiqyyomanet7m!8j_v4_l%qj^uaahj9ch'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'r*&5y9l$am*z5%nq&iiqyyomanet7m!8j_v4_l%qj^uaahj9ch')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
