0c7901a793f38071c6256f30f3972cb2331475e9
simjiwon <wldnjs0919@likelion.org>
2019-07-31 23:36:14 +0900

    finalfinalfinal

--- HTMLCalendar/settings.py
+++ HTMLCalendar/settings.py
@@ -126,3 +126,6 @@ STATIC_URL = '/static/'
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL =  '/media/'
